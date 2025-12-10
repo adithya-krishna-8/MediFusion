4e373578b2ab88abeb6b137f9e8e96458dffdef1
SyedEshaan-ctrl <eshaan2hyd@gmail.com>
2025-12-10 13:56:57 +0530

    Fix: Revert to REACT_APP_API_URL content in CRA

--- frontend/src/api/axiosClient.js
+++ frontend/src/api/axiosClient.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = (import.meta.env.VITE_API_URL || 'http://localhost/api').replace(/\/$/, '');
+const baseURL = (process.env.REACT_APP_API_URL || 'http://localhost/api').replace(/\/$/, '');
 
 const axiosClient = axios.create({
   baseURL: baseURL,
